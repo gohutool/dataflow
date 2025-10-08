b6696cdc0c0cf96a789731db81b2faca4e2e8db4
LiuYong <793875613@qq.com>
2025-10-08 22:44:33 +0800

    提交部分代码，还没有完成

--- dataflow/module/context/pybatisplus.py
+++ dataflow/module/context/pybatisplus.py
@@ -5,9 +5,26 @@ from dataflow.utils.reflect import inspect_own_method
 from dataflow.utils.utils import str_isEmpty,PageResult
 from dataflow.utils.dbtools.pydbc import PydbcTools
 from dataflow.utils.dbtools.pybatis import PageMode
-
+from typing import get_type_hints
+import inspect
 
 _logger = Logger('dataflow.module.context.pybatisplus')
+
+def _binding_function_with_pybatis(cls, func_name, func):
+    _logger.DEBUG(f'{func_name}.{func}')
+    
+    sig = inspect.signature(func)        
+    type_hints = get_type_hints(func)
+    return_type = type_hints.get('return')
+    
+        
+    def _sql_proxy(self, *args, **kwargs)->any:
+        bound = sig.bind_partial(self, *args, **kwargs)
+        bound.apply_defaults()        
+        _logger.DEBUG(f'{bound.arguments}=>{return_type}')
+        return func(self, *args, **kwargs)
+        
+    setattr(cls, func_name, _sql_proxy)
 
 def Mapper(datasource:str=None, *, table:str=None,id_col='id'):
     def mapper_decorator(cls):
@@ -49,13 +66,15 @@ def Mapper(datasource:str=None, *, table:str=None,id_col='id'):
         @classmethod
         def delete_by_id(cls, pk:any)->int:
             return getDataSource().deleteT(_table, {_id_col:pk})
-        
-        funcs = inspect_own_method(cls)
-        
-        for func in funcs:
-            _logger.DEBUG(f'{func[0]}.{func[1]}')
+            
         # def say(self, pk:any)->int:
-        #     print(f'=========={dir(self)} {_id_col}={pk}')
+        #     print(f'=========={dir(self)} {self.name}{_id_col}={pk}')
+        # cls.say = say
+        # _logger.DEBUG(f'say.{say}')            
+        
+        funcs = inspect_own_method(cls)        
+        for func in funcs:            
+            _binding_function_with_pybatis(cls, func[0], func[1])
 
         # 把方法挂到类上
         cls.select_by_id = select_by_id
--- testsuite/pybatisplusT.py
+++ testsuite/pybatisplusT.py
@@ -3,15 +3,27 @@ from dataflow.module.context.pybatisplus import Mapper
 
 
 @Mapper(table='sys_user',id_col='user_id')
-class TestMapper:
-    pass
-
-# 
+class TestMapper:    
+    def __init__(self, name:str='LiuYong'):
+        self.name = name
+        
+    def say(self, word)->str:
+        return f'{self.name} Say: {word}'
+    
+    def run(self, road)->str:
+        return f'{self.name} run: {road}'
 
 if __name__ == "__main__":
     print('== start')
-    t = TestMapper()
+    t = TestMapper('LiuYong')
     print(dir(t))
     
     print(t.select_by_id('2'))
-    t.say('Liuyong')
+    print(f'Say={t.say('Liuyong')}')
+    print(f'Run={t.run('Shenzhen')}')
+    
+    t = TestMapper('Dataflow')
+    print(t.select_by_id('2'))
+    print(f'Say={t.say('Liuyong')}')
+    print(f'Run={t.run('Shenzhen')}')
+    
