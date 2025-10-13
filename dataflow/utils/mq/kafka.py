f159ab88c9e2deb84db1118395396a1271e503c9
LiuYong <793875613@qq.com>
2025-10-13 13:15:47 +0800

    线程池封装

--- dataflow/utils/mq/kafka.py
+++ dataflow/utils/mq/kafka.py
@@ -1,38 +1,80 @@
-from confluent_kafka import Producer,Consumer
+from confluent_kafka import Producer ,Consumer
 import json
 import time
+from dataflow.utils.thread import newThread
+import atexit
 
-p = Producer({'bootstrap.servers': '192.168.18.145:9092'})
+def getProducer(config:dict)->Producer:
+    return Producer(config)
 
-def cb(err, msg):
-    if err:
-        print('❌', err)
-    else:
-        print('✅', msg.topic(), msg.partition(), msg.offset())
+def produce(producer:any, topic:str, playload:str, cb:callable):
+    producer.produce(topic, payload, callback=cb)
 
-for i in range(10):
-    print(f'==={i+1}')
-    payload = json.dumps({'id': i, 'ts': time.time()})
-    p.produce('python.test', payload, callback=cb)
-    p.poll(0)          # 触发回调
-print('====end')
-p.flush()
-print('flush end')
+def getConsumer(config:dict)->any:
+    c = Consumer(config)
+    return c
 
-c = Consumer({
-    'bootstrap.servers': '192.168.18.145:9092',
-    'group.id': 'python-demo',
-    'auto.offset.reset': 'earliest'
-})
-c.subscribe(['python.test'])
+def subscribe(consumer:Consumer, topic:str, onConsumer:callable):        
+    obj = {
+        'is_running':True
+    }
+    
+    def startSubscribe():
+        while obj['is_running']:
+            msg = c.poll(1.0)
+            if msg is None: 
+                continue
+            if msg.error():
+                # print('⚠️', msg.error())
+                onConsumer(error=msg.error(), msg=msg)
+                continue
+            onConsumer(error=None, msg=msg)
+            # print('💬', msg.value().decode())
+            
+    t = newThread(startSubscribe, name=f'{consumer}-{topic}', daemon=True)
+    setattr(t, '__end__', obj)
+    t.start()
+    
+    def on_exit():
+        obj['is_running'] = False
+        
+    atexit.register(on_exit)
+    
+# p = Producer({
+#     'bootstrap.servers': '192.168.18.145:9092',
+#     'debug': 'all'
+#     })
 
-while True:
-    msg = c.poll(1.0)
-    if msg is None: continue
-    if msg.error():
-        print('⚠️', msg.error())
-        continue
-    print('💬', msg.value().decode())
+# def cb(err, msg):
+#     if err:
+#         print('❌', err)
+#     else:
+#         print('✅', msg.topic(), msg.partition(), msg.offset())
+
+# for i in range(10):
+#     print(f'==={i+1}')
+#     payload = json.dumps({'id': i, 'ts': time.time()})
+#     p.produce('python.test', payload, callback=cb)
+#     print(p.poll(0))          # 触发回调
+# print('====end')
+# # p.flush()
+# print('flush end')
+
+# c = Consumer({
+#     'bootstrap.servers': '192.168.18.145:9092',
+#     'group.id': 'python-demo',
+#     'auto.offset.reset': 'earliest'
+# })
+# c.subscribe(['python.test'])
+
+# while True:
+#     msg = c.poll(1.0)
+#     if msg is None: 
+#         continue
+#     if msg.error():
+#         print('⚠️', msg.error())
+#         continue
+#     print('💬', msg.value().decode())
 
 
 if __name__ == "__main__":
